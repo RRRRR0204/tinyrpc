0a5a49916a34cd2745151a7f3d24ece70eaf52fa
renzhuoli <1024047967@qq.com>
2023-03-15 22:40:29 +0800

    protobuf练习

---
+++ test/protobuf/main.cc
@@ -0,0 +1,45 @@
+#include "test.pb.h"
+#include <iostream>
+#include <string>
+using namespace fixbug;
+
+int main()
+{
+    // // 生成一个请求
+    // fixbug::LoginRequest req;
+    // req.set_name("zhang san");
+    // req.set_pwd("123456");
+
+    // std::string send_str;
+    // // 将请求序列化
+    // if (req.SerializeToString(&send_str))
+    // {
+    //     std::cout << send_str << std::endl;
+    // }
+
+    // // 将请求反序列化
+    // fixbug::LoginRequest reqB;
+    // if (reqB.ParseFromString(send_str))
+    // {
+    //     std::cout << reqB.name() << std::endl;
+    //     std::cout << reqB.pwd() << std::endl;
+    // }
+
+    GetFriendListRequestResponse rsp;
+    ResultCode *rc = rsp.mutable_result();
+    rc->set_errcode(0);
+
+    User *user1 = rsp.add_friend_list();
+    user1->set_name("zhang san");
+    user1->set_age(20);
+    user1->set_sex(User::MAN);
+
+    User *user2 = rsp.add_friend_list();
+    user2->set_name("zhang san");
+    user2->set_age(20);
+    user2->set_sex(User::MAN);
+
+    std::cout << rsp.friend_list_size() << std::endl;
+
+    return 0;
+}
